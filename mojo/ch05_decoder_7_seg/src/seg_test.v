9f59adf06e5d8be4826f096c766f945251cc48f5
Simon Monk <evilgeniusauthor@gmail.com>
2016-04-20 14:03:20 +0100

    initial commit

---
+++ mojo/ch05_decoder_7_seg/src/seg_test.v
@@ -0,0 +1,15 @@
+module seg_test(
+	input CLK,
+   input [3:0] D,
+   output [7:0] SEG,
+	output reg [3:0] DIGIT
+   );	
+
+always @(*) 
+begin
+	DIGIT = 4'b1110;
+end
+
+decoder_7_seg decoder(.CLK (CLK), .SEG	(SEG), .D (D));  
+
+endmodule
